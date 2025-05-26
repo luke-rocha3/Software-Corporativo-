58c453c5b73f1b24a67a63858864218cd419b276
luke-rocha3 <lucasrx2410@gmail.com>
2025-05-26 18:14:28 -0300

    requestParam e Pathvariable

--- demo/src/main/java/ifpe/edu/br/demo/Controlador/HelloWorldRestController.java
+++ demo/src/main/java/ifpe/edu/br/demo/Controlador/HelloWorldRestController.java
@@ -1,13 +1,32 @@
 package ifpe.edu.br.demo.Controlador;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
+@RequestMapping("/api/hello")
 public class HelloWorldRestController {
 
-     @GetMapping("/")
+    @GetMapping("/hello")
     public String hellooWorld() {
         return "Hello World!!";
     }
+
+    @GetMapping("/param")
+    public String helloWorld(@RequestParam String nome) {
+        return "Hello, " + nome + "!";
+        //http://localhost:8080/api/hello/param?nome=Joao
+
+    }
+
+    @GetMapping("/path/{nome}")
+    public String helloWorldPath(@PathVariable String nome) {
+        return "Hello, " + nome + "!";
+        //http://localhost:8080/api/hello/path/?
+
+    }
+
 }
